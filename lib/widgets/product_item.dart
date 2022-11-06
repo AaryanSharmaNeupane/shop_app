504768b9cd01f68306cd7ff845dd7a268f33eaaf
Aaryan Neupane <sonofnaleen@gmail.com>
2022-11-06 09:12:11 +0545

    home screen added

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String imageUrl;
+  final String title;
+  final double price;
+
+  ProductItem(this.id, this.title, this.imageUrl, this.price);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.contain,
+        ),
+        header: GridTileBar(
+          backgroundColor: Colors.black87,
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 14,
+              overflow: TextOverflow.fade,
+            ),
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: const Icon(Icons.favorite),
+            onPressed: () {},
+            color: Theme.of(context).secondaryHeaderColor,
+          ),
+          title: Text(
+            "\$${price.toStringAsFixed(2)}",
+            style: const TextStyle(
+              fontSize: 12,
+              overflow: TextOverflow.fade,
+            ),
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            onPressed: () {},
+            color: Theme.of(context).secondaryHeaderColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
